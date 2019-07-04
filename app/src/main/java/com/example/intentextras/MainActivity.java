0899eec647d1413bef7ad46addcdfbabfb639d3b
bolucian <wambuilucian@gmail.com>
2019-07-04 22:12:51 +0300

    Update

--- app/src/main/java/com/example/intentextras/MainActivity.java
+++ app/src/main/java/com/example/intentextras/MainActivity.java
@@ -1,26 +1,21 @@
+
 package com.example.intentextras;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etName;
     EditText etAge;
     Button btnSend;
-
+    String name;
+    String age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,53 +23,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        etName=findViewById(R.id.etName);
-        etAge=findViewById(R.id.etAge);
-        btnSend=findViewById(R.id.btnSend);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        etName = findViewById(R.id.etName);
+        etAge = findViewById(R.id.etAge);
+        btnSend = findViewById(R.id.btnSend);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent myIntent= new Intent(getBaseContext(),RecieverActivity.class);
-                myIntent.putExtra("value1",9);
-                myIntent.putExtra("value2",10);
+            public void onClick(View v) {
+
+                name = etName.getText().toString();
+                age = etAge.getText().toString();
+                Intent myIntent = new Intent(getBaseContext(),ReceiverActivity.class);
+                String name = etName.getText().toString();
+                String age = etAge.getText().toString();
+                myIntent.putExtra("etName",name);
+                myIntent.putExtra("etAge",age);
                 startActivity(myIntent);
 
 
+                FloatingActionButton fab = findViewById(R.id.fab);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent());
 
+
+                    }
+                });
             }
         });
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
--- app/src/main/java/com/example/intentextras/ReceiverActivity.java
+++ app/src/main/java/com/example/intentextras/ReceiverActivity.java
@@ -1,45 +1,33 @@
 package com.example.intentextras;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
 
 public class ReceiverActivity extends AppCompatActivity {
     TextView tvName;
     TextView tvAge;
+    String name;
+    String age;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        tvName=findViewById(R.id.tvName);
-        tvAge=findViewById(R.id.tvAge);
-        String savedExtra = getIntent().getStringExtra("value1");
-        tvName.setText(savedExtra);
-        int savedExtraNumber = getIntent().getIntExtra("value2",1);
-        tvAge.setText(savedExtraNumber);
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_receiver);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Bundle extras = getIntent().getExtras();
+        if (extras !=null);
+        {
+            String names = extras.getString("etName");
+            String ages = extras.getString("etAge");
+            tvName.setText(name);
+            tvAge.setText(age);
+        }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
-}
+
